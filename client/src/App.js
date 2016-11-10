815227d8d75127f62d790ea2698c7bbe8f57307a
Arnaud Didry <arnaud@fizix.io>
2016-11-11 00:55:41 +0100

    Refactor App
    use fetch()
    use new Button

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,45 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
-class Circle extends Component{
-  render() {
-    return (
-      <div className={`bell ${this.props.active ? 'belling' : '' }`}
-        onTouchStart={this.props.onTouchStart}
-        onTouchEnd={this.props.onTouchEnd}
+import Button from './Button';
 
-        onMouseDown={this.props.onTouchStart}
-        onMouseUp={this.props.onTouchEnd}
-      />
-    )
-  }
-}
-
-Circle.propTypes = {
-  onTouchStart: React.PropTypes.func,
-  onTouchEnd: React.PropTypes.func,
-  active: React.PropTypes.bool,
+const getButtonState = ({ isOn, isDown }) => {
+  return isOn ? 'on' :
+         isDown ? 'down' :
+         'off';
 }
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+  state = {
+    isDown: false,
+    isOn: false,
+  };
 
-    this.state = {
-      active: false
-    };
+  onTouchStart = event => {
+    event.preventDefault();
+    this.setState({ isDown: true });
+
+    fetch('/open', { method: 'POST', body: '' })
+      .then(() => {
+        this.setState({ isOn: true });
+        setTimeout(() => this.setState({ isOn: false }), 1000);
+      });
   }
 
-  openDoor = () => {
-    this.setState({ ...this.state, active: true });
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://192.168.1.26/open', true);
-    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xhr.send('');
-  }
-
-  stop = () => {
-    this.setState({ ...this.state, active: false });
+  onTouchEnd = event => {
+    event.preventDefault();
+    this.setState({ isDown: false });
   }
 
   render() {
     return (
-      <div className="App layout">
-        <Circle
-          onTouchStart={this.openDoor}
-          onTouchEnd={this.stop}
-          active={this.state.active}
+      <div className="App">
+        <Button
+          onTouchStart={this.onTouchStart}
+          onTouchEnd={this.onTouchEnd}
+          buttonState={getButtonState(this.state)}
         />
       </div>
     );
